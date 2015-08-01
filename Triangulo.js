0616aa603e71c80593a175b87b51a93ff83d9595
jardelcorrea <jardelwebdeveloper@gmail.com>
2015-08-01 17:15:46 -0300

    Triangulo
    Descubra o valor

---
+++ Triangulo.js
@@ -0,0 +1,20 @@
+function chamar (){
+ 
+ var idade=12;
+
+ if(idade==10)
+ {
+   alert("ele tem 10 anos de idade");
+ }
+
+  else if(idade==11)
+  {
+   alert("Ele tem 11 anos de idade");
+  }
+  
+  else
+  {
+    alert("Ele não tem nem 10 nem 11");
+  }
+
+};
